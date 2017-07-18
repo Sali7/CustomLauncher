3bfb03ed30b430edba2bcabb675b12359e79923a
MOHAMED <bouabane.medsalah@gmail.com>
2017-07-18 13:15:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/iav_versuchsrechner/customlauncher/AppDetail.java
@@ -0,0 +1,13 @@
+package com.example.iav_versuchsrechner.customlauncher;
+
+/**
+ * Created by IAV-Versuchsrechner on 18.07.2017.
+ */
+
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
